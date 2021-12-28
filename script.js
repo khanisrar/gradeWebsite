b7ca73d659f56d60cc321e9fc6c61c701776c8da
Israr Khan <khan.israr.lap.7651@gmail.com>
2021-12-28 19:12:22 +0530

    some changes

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+const showData = document.querySelector('.showData');
+
+showData.style.display = 'none';
+const physics = document.querySelector('#phy')
+const chemistry = document.querySelector('#che')
+const math = document.querySelector('#math')
+const hindi = document.querySelector('#hin')
+const english = document.querySelector('#eng')
+const subjects = [physics, chemistry, math, hindi, english];
+
+const submitButton = document.querySelector('.submitButton')
+
+
+
 const calcy = () => {
+
+    if (isAllSubjectMarkEntered()) {
+        console.log('proceed every mark is there')
+    } else {
+        console.log('please input some subjects  marks')
+        return;
+    }
+
     let Phy = document.getElementById('phy').value;
     let Che = document.getElementById('che').value;
     let Math = document.getElementById('math').value;
@@ -22,7 +44,50 @@ const calcy = () => {
     } else {
         Grade = "F";
     }
-    document.getElementById('showData').innerHTML = `Total = ${totalGrade} <br> Percentage = ${perc}%. <br> Grade = ${Grade} `
+    showData.style.display = 'block'
+    showData.innerHTML = `Total = ${totalGrade} <br> Percentage = ${perc}%. <br> Grade = ${Grade} `
 
 
+}
+
+subjects.forEach((subject) => {
+    subject.addEventListener('input', (e) => {
+        if (subject.value.length === 2) {
+            let indexOfCurrentSubject = subjects.indexOf(subject);
+            if (indexOfCurrentSubject < subjects.length - 1) {
+                subjects[indexOfCurrentSubject + 1].focus();
+            }
+            if (indexOfCurrentSubject === subjects.length - 1) {
+                submitButton.focus();
+            }
+        }
+    })
+})
+
+// const subjects = [physics, chemistry, math, hindi, english];
+
+// for (let i = 0; i < subjects.length - 1; i++) {
+//     let subject = subjects[i];
+//     subject.addEventListener('input', (e) => {
+//         if (subject.value.length === 2) {
+//             subjects[i + 1].focus();
+//         }
+//     })
+// }
+
+
+function isAllSubjectMarkEntered() {
+    let canWeProceed = true;
+    // subjects.forEach((subject) => {
+
+    // })
+    for (let subject of subjects) {
+        if (Number(subject.value) === 0) {
+            subject.focus();
+
+            canWeProceed = false;
+            break;
+        }
+    }
+    return canWeProceed;
 }
